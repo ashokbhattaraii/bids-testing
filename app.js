d928127aaad7454d863f019c00e70ea0ac73ab59
Raktim Shrestha <raktim@rumsan.com>
2022-01-24 16:15:13 +0545

    code cleanup

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ app.use(helmet());
 app.use(logger("dev"));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-app.use(express.urlencoded({}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
