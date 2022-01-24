67b3991eb57dde856d9e7498e9c1a4825addaa08
Raktim Shrestha <raktim@rumsan.com>
2022-01-24 16:14:52 +0545

    app limit extend to 50mb

--- app.js
+++ app.js
@@ -23,8 +23,9 @@ app.set("view engine", "ejs");
 
 app.use(helmet());
 app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.urlencoded({}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
