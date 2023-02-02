97ea77e11fbb3ae50745ebbccb34903a1a15a575
Prithvi Maharjan <prithvi.maharjan@rumsan.net>
2023-02-02 12:41:53 +0545

    Redirected to donors list after donor details edit

--- client/js/donor/edit.panel.js
+++ client/js/donor/edit.panel.js
@@ -51,7 +51,7 @@ class DonorEdit extends Panel {
     let resData = await Service.edit(donorId, data);
     Notify.show("Donor Data Updated Successfully");
     setTimeout(function () {
-      window.location.reload();
+      window.location.replace('/donors');
     }, 1000);
   }
 }
