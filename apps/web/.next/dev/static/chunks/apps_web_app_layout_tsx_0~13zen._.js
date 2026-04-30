dd9d8c7172d39796e9f048d096a21ff7ae94f78b
ashokbhattaraii <ashok.bhattarai@rumsan.net>
2026-04-30 15:34:20 +0545

    chore: remove node_modules and build artifacts from tracking, update .gitignore

--- apps/web/.next/dev/static/chunks/apps_web_app_layout_tsx_0~13zen._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__0cyxa4g._.css","static/chunks/node_modules__pnpm_07ix~yt._.js","static/chunks/apps_web_lib_0eebkqi._.js"],
-    source: "dynamic"
-});
--- apps/web/.next/dev/static/chunks/apps_web_app_page_tsx_04jjyma._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/apps_web_0v-b.2k._.js","static/chunks/node_modules__pnpm_0icnjj1._.js"],
-    source: "dynamic"
-});
