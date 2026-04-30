7590f728b6249374a4c905bd11d63e596c44087a
ashokbhattaraii <ashok.bhattarai@rumsan.net>
2026-04-30 16:29:07 +0545

    chore: update folder structure

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from "./app.module"
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors()
-  const port = process.env.PORT ?? 3001
+  const port = process.env.PORT ?? 3005
   await app.listen(port)
   console.log(`[api] listening on http://localhost:${port}`)
 }
